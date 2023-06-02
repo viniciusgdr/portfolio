43c64579035822842cb606e7ab1bbab5d476fc03
Vinicius Silva <79378773+viniciusgdr@users.noreply.github.com>
2023-06-02 19:51:42 -0300

    Initial commit from Create Next App

---
+++ pages/_app.tsx
@@ -0,0 +1,6 @@
+import '@viniciusgdr/styles/globals.css'
+import type { AppProps } from 'next/app'
+
+export default function App({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />
+}
