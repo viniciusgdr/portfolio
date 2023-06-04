852658b88ca378e6c0b2742bca4411da21570e2e
Vinicius Silva <79378773+viniciusgdr@users.noreply.github.com>
2023-06-04 17:03:31 -0300

    init portfolio

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,118 +1,172 @@
 import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import { NextRouter, useRouter } from 'next/router'
+import { FaSitemap, FaDesktop, FaLocationArrow, FaInstagram, FaWhatsapp, FaGithub, FaAddressBook, FaUserTie, FaHome, FaEnvelope, FaBriefcase } from 'react-icons/fa'
 
-const inter = Inter({ subsets: ['latin'] })
-
-export default function Home() {
+export const Index = () => {
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className="flex justify-center h-screen bg-[url('/home-background.jpg')] bg-no-repeat image-full bg-center	bg-cover">
+      <div id="lines" className='absolute h-screen w-[80vw] top-0 left-0 right-0 m-auto'>
+        <div className="line opacity-20 ml-[-50%] absolute h-screen w-[1px] top-0 left-[50%] bg-white overflow-hidden"></div>
+        <div className="line opacity-20 absolute h-screen w-[1px] top-0 left-[50%] bg-white overflow-hidden"></div>
+        <div className="line opacity-20 ml-[-25%] absolute h-screen w-[1px] top-0 left-[50%] bg-white overflow-hidden"></div>
+        <div className="line opacity-20 ml-[25%] absolute h-screen w-[1px] top-0 left-[50%] bg-white overflow-hidden"></div>
+        <div className="line opacity-20 ml-[50%] absolute h-screen w-[1px] top-0 left-[50%] bg-white overflow-hidden"></div>
+      </div>
+      <div className="text-center-view text-center self-center pl-32">
+        <h1 className="text-8xl font-bold text-white pb-8">Carlos Vinicius</h1>
+        <div className="cd-headline rotate-1">
+          <span className="cd-words-wrapper text-2xl">
+            <b>Desenvolvedor Web</b>
+          </span>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div className="absolute pt-16 right-20">
+        <div className="flex flex-col justify-center h-full">
+          <div className="flex justify-center">
+            <FaInstagram />
+          </div>
+          <div className="flex justify-center mt-3">
+            <FaWhatsapp />
+          </div>
+          <div className="flex justify-center mt-3">
+            <FaGithub />
+          </div>
+        </div>
       </div>
+    </div>
+  )
+}
+export const getGreetings = () => {
+  const date = new Date()
+  const hours = date.getHours()
+  let greetings = ''
+  if (hours >= 0 && hours < 12) {
+    greetings = 'Bom dia'
+  } else if (hours >= 12 && hours < 18) {
+    greetings = 'Boa tarde'
+  } else {
+    greetings = 'Boa noite'
+  }
+  return greetings
+}
+export default function Home() {
+  const router = useRouter()
+  return (
+    <>
+      <nav className="header-main fixed w-[10%] h-screen bg-[rgba(14,15,16,.6)] top-0">
+        <h2 className='pt-8 text-center text-4xl'>CV</h2>
+        <div className="flex flex-col h-full pt-16">
+          <div className="flex justify-center">
+            <a href="#home" onClick={
+              (e) => {
+                e.preventDefault()
+                router.push('/')
+              }
+            }>
+              <FaHome size={25} />
+            </a>
+          </div>
+          <div className="flex justify-center mt-10">
+            <a href="#about" onClick={
+              (e) => {
+                e.preventDefault()
+                router.push('/#about')
+              }
+            }>
+              <FaUserTie size={25} />
+            </a>
+          </div>
+          <div className="flex justify-center mt-10">
+            <a href="#resumo" onClick={
+              (e) => {
+                e.preventDefault()
+                router.push('/#resumo')
+              }
+            }>
+              <FaAddressBook size={25} />
+            </a>
+          </div>
+          <div className="flex justify-center mt-10">
+            <a href="#portfolio" onClick={
+              (e) => {
+                e.preventDefault()
+                router.push('/#about')
+              }
+            }>
+              <FaBriefcase size={25} />
+            </a>
+          </div>
+          <div className="flex justify-center mt-10">
+            <a href="#contact" onClick={
+              (e) => {
+                e.preventDefault()
+                router.push('/#contact')
+              }
+            }>
+              <FaEnvelope size={25} />
+            </a>
+          </div>
+        </div>
+      </nav>
+      {
+        router.asPath === '/#home' || router.asPath === '/' ? <Index /> :
+          <>
+            <div className="flex justify-center h-screen ">
+              <section className="container pr-[15px] pl-[15px] max-w-3xl">
+                <div className="row mt-[70px] mb-[70px] text-center text-6xl">
+                  <h2>Sobre mim</h2>
+                </div>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+                <div className="bg-[#070708] flex flex-col border rounded-lg mb-[30px] pt-4 pb-4 items-center text-center">
+                  <Image
+                    alt='Foto de perfil'
+                    src="https://img.kalunga.com.br/fotosdeprodutos/479735z_1.jpg"
+                    className="rounded-full bg-center border-4 border-white"
+                    width={200}
+                    height={200}
+                  />
+                  <div className="pl-4 self-center pt-4">
+                    <h2 className='text-2xl pb-2'>Desenvolvedor Web</h2>
+                    <h3 className='text-blue-500 flex items-center justify-center'>
+                      <FaLocationArrow />{'  '}
+                      Camaragibe - PE
+                    </h3>
+                    <p className='max-w-xl pt-3'>🖥️ Muito {getGreetings()}! Me chamo Carlos Vinicius, tenho 15 anos. Meu foco de atuação é no segmento back end 🚀</p>
+                  </div>
+                </div>
+                <div className="bg-[#070708] flex flex-col border rounded-lg pt-4 pb-4 text-center">
+                  <div className="grid grid-cols-2">
+                    <div className=" text-center place-self-center">
+                      <h1 className='text-2xl'>Freelancer</h1>
+                      <a className='text-blue-500'>Disponível</a>
+                    </div>
+                    <div className="text-center place-self-center">
+                      <p className='text-2xl'>Curriculum</p>
+                      <a href="/curriculum.pdf" target="_blank" rel="noopener noreferrer" className='text-blue-500'>Baixar</a>
+                    </div>
+                  </div>
+                </div>
+                <div className="row mt-[70px] mb-[30px] text-center text-4xl">
+                  <h2>Serviços</h2>
+                </div>
+                <div className="bg-[#070708] border rounded-lg pt-2 pb-4 text-center">
+                  <div className="grid grid-cols-2 p-4 grid-flow-col">
+                    <div className="flex flex-col text-center place-self-center items-center">
+                      <FaDesktop size={50} />
+                      <h1 className='text-2xl'>Desenvolvimento Web</h1>
+                      <a>Desenvolvimento de sistemas para internet escaláveis, com qualidade e personalização. Sempre de olho em novas tendências e utilizando as melhores tecnologias do mercado sempre alinhadas com os seus objetivos.</a>
+                    </div>
+                    <div className="flex flex-col text-center place-self-center items-center">
+                      <FaSitemap size={50} />
+                      <p className='text-2xl'>Criação de Sites</p>
+                      <a>Sites profissionais, personalizados e responsivos de alta performance e otimizados para o Google. Aumente a visibilidade do seu negócio na internet e gere credibilidade para a sua empresa.</a>
+                    </div>
+                  </div>
+                </div>
+              </section>
+            </div>
+          </>
+      }
+    </>
   )
 }
